34536e655cda21a5aaa4cb3a3cc89a000b026f32
shuffle10 <saphal.pant@westcliff.edu>
2023-04-15 10:17:46 +0545

    increase timeout

--- controller/photographerController.js
+++ controller/photographerController.js
@@ -2,7 +2,7 @@ import User from "../models/models.js";
 
 export const fetchUsers = async (request, response) => {
   try {
-    let users = await User.find({});
+    let users = await User.find({}).maxTimeMS(30000);
     response.status(200).json(users);
   } catch (err) {
     response.status(404).json({ message: err.message });
@@ -11,7 +11,7 @@ export const fetchUsers = async (request, response) => {
 
 export const fetchUser = async (request, response) => {
   try {
-    let user = await User.findById(request.params.id);
+    let user = await User.findById(request.params.id).maxTimeMS(30000);
     response.status(200).json(user);
   } catch (err) {
     response.status(404).json({ message: err.message });
